dea03704250678cca62d7ad7bad698831bb3d0fc
Henry <henryasante336@gmail.com>
2025-06-24 07:59:58 +0000

    Made some hosting changes

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 //API URL
-export const BASE_URL = 'http://localhost:5000/api/';
+export const BASE_URL = 'https://trimz-backend.onrender.com/api/';
 export const token = localStorage.getItem('token');
