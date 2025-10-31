89d8f01c6d63c58a3044373c161f1ae810ee482a
Henry <henryasante336@gmail.com>
2025-10-31 02:22:57 +0000

    update API URL to point to the correct backend instance

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 //API URL
-export const BASE_URL = 'https://trimz-backend.onrender.com/api/';
+export const BASE_URL = 'https://trimz-backend-fo3j.onrender.com/api/';
 export const token = localStorage.getItem('token');
